d87446e0adf2cc1c1af22c265aa6adc3ee176c7e
Sanket Atish Chaudhari <sanketchaudhari1264@gmail.com>
2026-03-07 11:30:27 +0530

    Update index.js

--- tyrex-backend/index.js
+++ tyrex-backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://tyrexuser:Tyrex%4012345@cluster0.kfdws64.mongodb.net/tyrexdb?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully ✅"))
   .catch((err) => console.error("MongoDB connection error ❌", err));
 
